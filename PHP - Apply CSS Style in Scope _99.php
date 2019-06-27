a3bf3e3313f17335339cfb55ac051e32e335b57b
coder4589 <GeneralUse4589@outlook.com>
2019-06-27 13:45:42 -0100

    Create PHP - Apply CSS Style in Scope _99.php

---
+++ PHP - Apply CSS Style in Scope _99.php
@@ -0,0 +1,24 @@
+<style> #El_<?php echo @++$Unique_Id ?> {background-color: coral;} #El_<?php echo @$Unique_Id ?> p {color:red;}  </style>
+<div id="El_<?php echo @$Unique_Id ?>"> 
+
+<p>1</p> <p>2</p> <p>3</p>
+
+    <style> #El_<?php echo @++$Unique_Id ?> {background-color: blue;} #El_<?php echo @$Unique_Id ?> p {color:white;}  </style>
+    <div id="El_<?php echo @$Unique_Id ?>">
+
+    <p>a</p> <p>b</p> <p>c</p>
+
+              <style> #El_<?php echo @++$Unique_Id ?> {background-color: black;} #El_<?php echo @$Unique_Id ?> p {color:green;}  </style>
+              <div id="El_<?php echo @$Unique_Id ?>">
+                  
+              <p>x</p><p>y</p><p>z</p>
+              
+              </div>
+
+    <p>d</p> <p>e</p> <p>f</p>
+
+    </div>
+
+<p>4</p> <p>5</p> <p>6</p>
+
+</div>
